92303f3906ad9b379c7d01b5bc2859b022a55e87
MaximeGillot5 <mgillot56@icloud.com>
2023-10-28 15:20:27 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require('dotenv').config()
+const port = 5000
+
+connectDB();
+
+const app = express();
+
+// app.get("/post", (req, res) => {
+//     res.json({ message: "Voici les données" });
+// });
+
+//Middleware qui permet de traiter les données de la request
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use("/post", require("./routes/post.routes"))
+
+
+//Lancer le serveur
+app.listen(port, () => console.log("Le serveur a démarré au port  " + port))
